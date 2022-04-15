8efeb13c078a92e319aea384b13613cde704fc2c
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-04-15 16:02:07 +0100

    this is an inventory app, that uses the room android features.

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,20 @@
+package com.example.inventory.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.text.NumberFormat
+import java.util.*
+
+@Entity(tableName = "item")
+data class Item(@PrimaryKey(autoGenerate = true)
+                val id: Int = 0,
+                @ColumnInfo(name = "name")
+                val itemName: String,
+                @ColumnInfo(name = "price")
+                val itemPrice: Double,
+                @ColumnInfo(name = "quantity")
+                val quantityInStock: Int) {
+}
+
+fun Item.getFormattedPrice(): String = NumberFormat.getCurrencyInstance(Locale("en", "NG")).format(itemPrice)
